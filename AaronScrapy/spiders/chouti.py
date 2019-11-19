fe18f57c5065a298e78233b2fe4e02c7507ff43f
Aaron.Yang <1692134188@qq.com>
2019-11-19 17:17:59 +0800

    Hello Scrapy

---
+++ AaronScrapy/spiders/chouti.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+# import sys
+# import io
+
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
+
+# A：首先我们需要创建一个类，并继承scrapy的一个子类：scrapy.Spider  或者是其他蜘蛛类型，后面会说到，除了Spider还有很多牛X的蜘蛛类型；
+class ChoutiSpider(scrapy.Spider):
+    name = 'chouti' # B：然后定义一个蜘蛛名，name=“”
+    allowed_domains = ['chouti.com']
+    start_urls = ['http://dig.chouti.com/'] # C：定义我们需要爬取的网址，没有网址蜘蛛肿么爬
+
+    def parse(self, response):
+        print(response)
+        print("Hello,Scrapy")
